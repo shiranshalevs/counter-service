36022549a0ee30b6e60edca7dd1b20c0edbc06bd
Shiran Shalev <shirans@boltinc.com>
2023-03-27 01:29:18 +0300

    test

--- test.js
+++ test.js
@@ -6,9 +6,14 @@ after(() => {
     process.exit();
   });
 
-describe('App', () => {
-  it('should respond with a 200 status code', async () => {
-    const res = await request(app).get('/');
-    expect(res.status).to.equal(500);
+  describe('App', () => {
+    it('should respond with a 200 status code', async () => {
+      try {
+        const res = await request(app).get('/');
+        expect(res.status).to.equal(500);
+      } catch (err) {
+        console.error(err);
+        process.exit(1);
+      }
+    });
   });
-});
