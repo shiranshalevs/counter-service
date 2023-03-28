7bfd9a315e30059a319fba8c37a2f165a92a4564
Shiran Shalev <shirans@boltinc.com>
2023-03-28 03:23:29 +0300

    remove lowdb

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
 
+let counter = 0;
 
-// Initialize the database
-const adapter = new FileSync('db.json');
-const db = low(adapter);
-
-// Set up the counter in the database
-db.defaults({ counter: 0 }).write();
-
-
-// Update the counter in the database on each POST request
+// Update the counter on each POST request
 app.post('/', (req, res) => {
-  db.update('counter', n => n + 1).write();
+  counter++;
   res.sendStatus(200);
 });
 
-// Get the counter value from the database on each GET request
+// Get the counter value on each GET request
 app.get('/', (req, res) => {
-  const counter = db.get('counter').value();
   console.log(`Counter retrieved as ${counter}`);
   res.send(`The counter is currently at ${counter}`);
 });
 
-  app.listen(80, () => {
-    console.log('Counter service listening on port 80');
-  });
+app.listen(80, () => {
+  console.log('Counter service listening on port 80');
+});
 
-  module.exports = app;
+module.exports = app;
